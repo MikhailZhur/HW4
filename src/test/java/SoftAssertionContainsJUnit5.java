78792442ba20ad3aab87b6d237d92fb61cf45218
zhuravlev.mikhail <miha99_66@mail.ru>
2023-03-06 10:09:39 +0300

    last version

--- src/test/java/SoftAssertionContainsJUnit5.java
+++ src/test/java/SoftAssertionContainsJUnit5.java
@@ -1,2 +1,33 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SoftAssertionContainsJUnit5 {
+    String sampleJUnitCode = "@ExtendWith({SoftAssertsExtension.class})\n" +
+            "class Tests {\n" +
+            "  @Test\n" +
+            "  void test() {\n" +
+            "    Configuration.assertionMode = SOFT;\n" +
+            "    open(\"page.html\");\n" +
+            "\n" +
+            "    $(\"#first\").should(visible).click();\n" +
+            "    $(\"#second\").should(visible).click();";
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.screenshots = false;
+        Configuration.baseUrl = "https://github.com/";
+    }
+    @Test
+    void sampleCodeForJUnit5Tests() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-body").shouldHave(text("Soft assertions"));
+        $(byText("Soft assertions")).click();
+        $("#wiki-body").shouldHave(text(sampleJUnitCode));
+    }
 }
